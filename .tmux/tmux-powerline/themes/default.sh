38eca211d6e439dfdcd5685fcce9ff267da6c049
Piyoshi <usususus3104@gmail.com>
2014-09-06 11:13:13 +0900

    update tmux powerline color

--- .tmux/tmux-powerline/themes/default.sh
+++ .tmux/tmux-powerline/themes/default.sh
@@ -29,12 +29,14 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		"hostname 234 255" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		"wan_ip 24 255" \
-		"vcs_branch 22 135" \
-		"vcs_compare 16 255" \
-		"vcs_staged 18 250" \
-		"vcs_modified 103 17" \
+		"lan_ip 19 255" \
+		# "lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		# "wan_ip 24 255" \
+		"vcs_branch 130 232" \
+		# "vcs_branch 22 135" \
+		# "vcs_compare 16 255" \
+		# "vcs_staged 18 250" \
+		# "vcs_modified 103 17" \
 		#"vcs_others 245 0" \
 	)
 fi
@@ -55,7 +57,7 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"weather 37 255" \
 		#"rainbarf 0 0" \
 		# "xkb_layout 125 117" \
-		"xkb_layout 235 117" \
+		# "xkb_layout 235 117" \
 		"date_day 88 136" \
 		"date 88 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}" \
 		"time 88 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}" \
